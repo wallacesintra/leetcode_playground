0ce42b147d01d79dc5a3f70cc9db7a6b8502be31
wallacesintra <wallvxwallace962@gmail.com>
2025-02-20 22:56:38 +0300

    initial commit

---
+++ app/src/androidTest/java/com/example/leetcode_playground/binary_search/BinarySearchTest.kt
@@ -0,0 +1,15 @@
+package com.example.leetcode_playground.binary_search
+
+import org.junit.Test
+
+class BinarySearchTest {
+    @Test
+    fun `doesBinarySearchReturnInt` (){
+        //given
+        val nums = emptyList<Int>()
+        val target = 1
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/leetcode_playground/binary_search/BinarySearch.kt
@@ -0,0 +1,5 @@
+package com.example.leetcode_playground.binary_search
+
+fun binary_search(nums: List<Int>, target: Int): Int?{
+    return null
+}
